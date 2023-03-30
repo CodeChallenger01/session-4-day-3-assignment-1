cae02253bed95ddca4d9dcd5af7fa990f9797231
Manish Mishra <manish.mishra@knoldus.com>
2023-03-30 23:15:39 +0530

    Added Exception handling

--- payment-system/src/main/scala/com/knoldus/Driver.scala
+++ payment-system/src/main/scala/com/knoldus/Driver.scala
@@ -2,6 +2,7 @@
 package com.knoldus
 
 import scala.io.StdIn.readLine
+import scala.util.{Failure, Success, Try}
 
 object Driver extends App {
   private val paymentObject = new PaymentMethod
@@ -13,6 +14,10 @@ object Driver extends App {
   print("\n\nView what are the payments option available for user:\n" + viewPaymentMethod)
 
   private val processPaymentType: String = readLine("\n\nEnter Only this payment type :\n1.credit-card\n2.paypal\n3.bank-transfer\nEnter :")
-  private val afterProcessingPayment = paymentObject.selectProcessPayment(listOfPaymentMethod, processPaymentType)
-  print("\n" + afterProcessingPayment)
+  private val afterProcessingPayment = Try(paymentObject.selectProcessPayment(listOfPaymentMethod, processPaymentType))
+  private val finalResult = afterProcessingPayment match {
+    case Success(value) => value
+    case Failure(exception) => exception.getMessage
+  }
+  print("\n" + finalResult)
 }
